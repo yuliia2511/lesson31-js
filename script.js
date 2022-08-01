eaf175a06f36f63c37fd6138df7c5a81c8e463a4
yuliia2511 <yuliyaaa.katanchyk@gmail.com>
2022-08-01 11:03:37 -0300

    commit on dev

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function checkLength() {
 }
 
 
-
 document.querySelector('.second_btn').onclick = getFactorial
 
 function getFactorial() {
@@ -18,6 +17,38 @@ function getFactorial() {
     }
     document.querySelector('.task2Answer').innerHTML = result
 }
+
+
+//выдает неправильный результат
+
+document.querySelector('.third_btn').onclick = getRow
+
+    function getRow() {
+        let a = document.querySelector('.num2').value; 
+        let b = (а) => a.join('');
+        document.querySelector('.task3Answer').innerHTML = b;
+    } 
+//
+
+
+document.querySelector('.forth_btn').onclick = findCommonNumber;
+
+function findCommonNumber() {
+
+    const arg = document.querySelector('.arg').value;
+    const arg2 = document.querySelector('.arg2').value;
+
+    if (arg < arg2) {
+        result = -1;
+    } else if (arg > arg2) {
+        result = 1;
+    } else if (arg == arg2) {
+        result = 0;
+    }
+    document.querySelector('.task4Answer').innerHTML = result;
+}
+
+
 
 
 
